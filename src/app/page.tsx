568a9b2599084dc8e9e56c01742f6cce8b127b42
Daniel <>
2024-11-01 15:44:17 +0000

    Components added: reusable card components for cruise and adventure, also another another folder has been added reasuable-cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import CruiseCard from "@/components/Reusable-cards/CruiseCard";
 
 export default function Home() {
   return (
---
+++ src/components/Reusable-cards/AdventureCards.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+interface AdventureCardProps {
+  name: string;
+  image: string;
+}
+
+const AdventureCard: React.FC<AdventureCardProps> = ({ name, image }) => {
+  return (
+    <div className="border rounded-lg shadow-md pt-4 bg-blue-50 w-64">
+      <div>
+        <Image
+          src={image}
+          alt={name}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg"
+        />
+      </div>
+      <h2 className="text-lg font-bold">{name}</h2>
+    </div>
+  );
+};
+export default AdventureCard;
