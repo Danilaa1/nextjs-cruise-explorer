11efabe56b5f2f33b28f179b137033e7242d309d
Daniel <>
2024-11-01 10:56:23 +0000

    Initial commit: Set up project folder structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({
+  weight: "300",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Find your adventure with us.",
+  description: "My kind of cruise app.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={outfit.className}>{children}</body>
+    </html>
+  );
+}
