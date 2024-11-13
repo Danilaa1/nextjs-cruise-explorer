be4c0feb4219b77946de6ef0c52e9ebf1cfb1602
Daniel <>
2024-11-13 19:27:15 +0000

    removed: space in my tailwincss class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           </div>
         </section>
         <h2 className="text-2xl font-normal mb-4">Popular Cruises</h2>
-        <div className="flex flex-row gap-3  overflow-x-auto flex-nowrap">
+        <div className="flex flex-row gap-3 overflow-x-auto flex-nowrap">
           {cruises.map((cruise, index) => (
             <CruiseCard
               key={`${cruise.name}-${index}`}
